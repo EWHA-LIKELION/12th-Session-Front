21b2251fbeacc562d3718fc4daafbfd321c64274
mod_siw <pvq_71@ewhain.net>
2024-05-02 12:31:37 +0900

    [240502] React 1 세션 자료

--- session/React_1/src/components/Name.js
+++ session/React_1/src/components/Name.js
@@ -1,14 +1,21 @@
 import React from "react";
 
 const Name = ({ name, nth, isFE }) => {
+  const style = {
+    backgroundColor: "black",
+    color: "orange",
+  };
   return (
     <>
       {isFE ? (
-        <div>
+        <div style={style}>
           {nth}기 프론트엔드 {name}입니다.
         </div>
       ) : null}
     </>
   );
+};
+Name.defaultProps = {
+  name: "변지혜",
 };
 export default Name;
