e7f3514e07fb17a082d42f1a64c23b7bb6bff93d
3olly <cy.kim@ewhain.net>
2024-05-30 18:19:14 +0900

    240530 세션 전 커밋

--- session/React_4/src/App.js
+++ session/React_4/src/App.js
@@ -3,27 +3,81 @@ import axios from "axios";
 
 function App() {
   // POST 예제 (CREATE)
-  const onPost = () => {};
+  const onPost = () => {
+    axios({
+      method: "Post",
+      url: "https://reqres.in/api/users",
+      data:{
+        "name": "morpheus",
+        "job": "leader"
+    },
+    })
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+      throw new Error(error);
+    });
+  };
 
   // GET 예제 (1) (LIST USERS)
-  const getFirst = () => {};
+  const getFirst = () => {
+    axios({
+      method: "Get",
+      url: "https://reqres.in/api/users?apge=2",
+      
+    })
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+      throw new Error(error);
+    });
+  };
   // GET 예제 (2) (SINGLE USER)
-  const getSecond = () => {};
+  const getSecond = () => {
+    axios({
+      method: "Get",
+      url: "https://reqres.in/api/users/2",
+      
+    })
+    .then((response) => {
+      console.log(response.data.data);
+    })
+    .catch((error) => {
+      console.log(error);
+      throw new Error(error);
+    });
+  };
   // GET 예제 (3) (SINGLE USER NOT FOUND)
-  const getThird = () => {};
+  const getThird = () => {
+    axios({
+      method: "Get",
+      url: "https://reqres.in/api/users/23",
+      
+    })
+    .then((response) => {
+      console.log(response.data.lastname);
+    })
+    .catch((error) => {
+      console.log(error);
+      throw new Error(error);
+    });
+  };
 
   return (
     <>
       <h1>POST</h1>
-      {/* POST 예제 */}
-      <h3>예제</h3>
+      <button onClick= {onPost}> POST 예제 </button>
       {/* GET */}
       <h1>GET</h1>
-      <h3>예제 1</h3>
+      <button onClick= {getFirst}> 예제 1</button>
       {/* GET 예제 2번 */}
-      <h3>예제 2</h3>
+      <button onClick= {getSecond}> 예제 2</button>
       {/* GET 예제 3번 */}
-      <h3>예제 3</h3>
+      <button onClick= {getThird}> 예제 3</button>
     </>
   );
 }
